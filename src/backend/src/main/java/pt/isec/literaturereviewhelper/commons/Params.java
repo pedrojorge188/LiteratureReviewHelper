fac2654d891869bff8080a0b9865180364d73d92
José Marcelo Marques da Cruz <jmarcelocruz99@outlook.com>
2025-10-30 08:09:50 +0000

    feat: add year parameter keys

--- src/backend/src/main/java/pt/isec/literaturereviewhelper/commons/Params.java
+++ src/backend/src/main/java/pt/isec/literaturereviewhelper/commons/Params.java
@@ -3,10 +3,12 @@ package pt.isec.literaturereviewhelper.commons;
 public final class Params {
     private Params() {}
 
-    public static final String QUERY   = "q";
-    public static final String START   = "start";
-    public static final String ROWS    = "rows";
-    public static final String WT      = "wt";
-    public static final String API_KEY = "api_key";
+    public static final String QUERY             = "q";
+    public static final String START             = "start";
+    public static final String ROWS              = "rows";
+    public static final String WT                = "wt";
+    public static final String API_KEY           = "api_key";
     public static final String DEEP_SEARCH_LIMIT = "deep_search_limit";
+    public static final String YEAR_START        = "year_start";
+    public static final String YEAR_END          = "year_end";
 }

73c59592f3d30fca8146ef11784023deaed890d6
Rodrigo Duarte <rodrigoduarte050@gmail.com>
2025-10-31 14:12:01 +0000

    Update build type to jar
    Now we will build jar instead of war for deployment purposes

--- src/backend/src/main/java/pt/isec/literaturereviewhelper/LiteratureReviewHelperApplication.java
+++ src/backend/src/main/java/pt/isec/literaturereviewhelper/LiteratureReviewHelperApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LiteratureReviewHelperApplication {
 
     public static void main(String[] args) {
-        // Disable native transport (Epoll/KQueue) and force NIO
         System.setProperty("reactor.netty.native", "false");
         
         SpringApplication.run(LiteratureReviewHelperApplication.class, args);

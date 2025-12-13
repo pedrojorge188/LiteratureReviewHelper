50c30c1d4cee5653c4c3211032fdce5b268f2332
José Marcelo Marques da Cruz <jmarcelocruz99@outlook.com>
2025-12-13 11:33:55 +0000

    feat: add navigation to quick start page

--- src/frontend/src/components/SideMenu.tsx
+++ src/frontend/src/components/SideMenu.tsx
@@ -47,7 +47,6 @@ export const SideMenu = () => {
                 {t("sideMenu:history")}
               </CommonLink>
             </div>
-
             <div className="side-menu__container__content-menu__link-container">
               <CommonLink
                 linkClass={`side-menu__container__content-menu__link-container__link ${location.pathname === `/${routes.libListPage.path}` ? "selected" : ""}`}
@@ -55,6 +54,15 @@ export const SideMenu = () => {
                 title={t("sideMenu:configuration")}
               >
                 {t("sideMenu:configuration")}
+              </CommonLink>
+            </div>
+            <div className="side-menu__container__content-menu__link-container">
+              <CommonLink
+                linkClass={`side-menu__container__content-menu__link-container__link ${location.pathname === `/${routes.quickStartPage.path}` ? "selected" : ""}`}
+                link={{ external: false, url: routes.quickStartPage.path }}
+                title={t("sideMenu:quickstart")}
+              >
+                {t("sideMenu:quickstart")}
               </CommonLink>
             </div>
           </div>
